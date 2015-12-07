5c8c8b027d06487c7761baaee3a30975b48775fd
arightious <arightious@gmail.com>
2015-12-07 17:16:21 +0000

    show feature and tests completed

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,9 +9,20 @@ def create
 		flash[:error] = 'Your entry was <strong>too short, or too long</strong>. Try again.'
 	end
 	redirect_to root_path
-end 
+end
+
+def about
+  end
+
+  def show
+    @quote = Quote.where(:id => params[:id]).first
+    if @quote.blank?
+      render :text => "You look lost...", :status => :not_found
+    end
+  end
 
 private
+
 def quote_params
 	params.require(:quote).permit(:saying, :author)
 end
