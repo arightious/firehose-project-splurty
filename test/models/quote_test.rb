205bd5bf67d868738b1967d7ae865c2cc2784868
arightious <arightious@gmail.com>
2015-12-07 03:33:26 +0000

    completed unique id feature w test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do
+    quote = Quote.create(:author => 'Franklin Roosevelt', :saying => 'A conservative is a man with two perfectly good legs who, however, has never learned how to walk forward.')
+    expected = 'FR#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
+
 end
